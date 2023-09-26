acb024f0c40c085fdd1a47175eb4f4b9e389d991
fdiello <felipediello@protonmail.com>
2023-09-26 20:59:18 -0300

    utilizando duas funções

--- Desafio 2/index2.js
+++ Desafio 2/index2.js
@@ -1,6 +1,8 @@
-let ranking = partidas(106,5)
-let niveisDeXp = ["Ferro", "Bronze", "Prata", "Ouro","Diamante", "Lendário", "Imortal"]
-let respostaFinal = "O herói tem o saldo de " + ranking + " vitórias e está no nível " 
+let ranking = partidas(30,5)
+let nivelDoHeroi= nivel(ranking)
+let respostaFinal = "O herói tem o saldo de " + ranking + " vitórias e está no nível " + nivelDoHeroi
+
+console.log(respostaFinal)
 
 
 function partidas(vitórias,derrotas){
@@ -8,24 +10,28 @@ function partidas(vitórias,derrotas){
     return saldoVitórias
 }
 
-if (ranking <=10) {
-    console.log(respostaFinal + niveisDeXp[0])
+function nivel(ranking){
+    let niveisDeXp = ["Ferro", "Bronze", "Prata", "Ouro","Diamante", "Lendário", "Imortal"]
+    
+    if (ranking <=10) {
+        return niveisDeXp[0]
 }
-else if(ranking>10 && ranking<=20){
-    console.log(respostaFinal+ niveisDeXp[1])
+    else if(ranking>10 && ranking<=20){
+        return niveisDeXp[1]
 }
-else if(ranking>20 && ranking<=50){
-    console.log(respostaFinal+ niveisDeXp[2])
+    else if(ranking>20 && ranking<=50){
+        return niveisDeXp[2]
 }
-else if(ranking>50 && ranking<=80){
-    console.log(respostaFinal+ niveisDeXp[3])
+    else if(ranking>50 && ranking<=80){
+        return niveisDeXp[3]
 }
-else if(ranking>80 && ranking<=90){
-    console.log(respostaFinal+ niveisDeXp[4])
+    else if(ranking>80 && ranking<=90){
+        return niveisDeXp[4]
 }
-else if(ranking>90 && ranking<=100){
-    console.log(respostaFinal+ niveisDeXp[5])
+    else if(ranking>90 && ranking<=100){
+        return niveisDeXp[5]
 }
-else if(ranking>100){
-    console.log(respostaFinal+ niveisDeXp[6])
+    else if(ranking>100){
+        return niveisDeXp[6]
+}
 }
